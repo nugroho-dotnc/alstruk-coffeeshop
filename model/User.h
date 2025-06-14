a40c071a82400a7253d74ba26a17d3a45deef366
sellolintang <sellolintang242@gmail.com>
2025-06-15 00:53:32 +0700

    add User

--- model/User.h
+++ model/User.h
@@ -1,31 +1,43 @@
-#pragma once
 #include <iostream>
 using namespace std;
-class User{
-    private:
+
+struct User {
     string name;
     string username;
     string password;
     string role;
-    public:
-     User(string name, string username, string password, string role) {
-        this->name = name;      
-        this->username = username;
-        this->password = password;
-        this->role = role;
-    }
-    // untuk inisiasi tanpa memasukkan parameter
-    User(){}
-    bool login(string username, string password){
-        if(this->username == username && this->password == password){
-            return true;
-        }
-        return false;
-    }
-    string getRole(){
-        return this->role;
-    }
-    string getName() {
-        return this->name;
-    }
 };
+
+const int TABLE_SIZE = 10;
+
+// #pragma once
+// #include <iostream>
+// using namespace std;
+// class User{
+//     private:
+//     string name;
+//     string username;
+//     string password;
+//     string role;
+//     public:
+//      User(string name, string username, string password, string role) {
+//         this->name = name;      
+//         this->username = username;
+//         this->password = password;
+//         this->role = role;
+//     }
+//     // untuk inisiasi tanpa memasukkan parameter
+//     User(){}
+//     bool login(string username, string password){
+//         if(this->username == username && this->password == password){
+//             return true;
+//         }
+//         return false;
+//     }
+//     string getRole(){
+//         return this->role;
+//     }
+//     string getName() {
+//         return this->name;
+//     }
+// };
